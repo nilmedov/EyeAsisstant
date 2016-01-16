3fff19b38f66a6a22557b589d33e3014849aa5f1
Nazar Ilmedov <nazar.ilmedov@ekreative.com>
2016-01-16 16:34:48 +0200

    init

---
+++ app/src/main/java/com/hackathon/healthtech/eyeassistant/MainActivity.java
@@ -0,0 +1,63 @@
+package com.hackathon.healthtech.eyeassistant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener {
+
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	private CameraBridgeViewBase mOpenCvCameraView;
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+				case LoaderCallbackInterface.SUCCESS: {
+					Log.i(TAG, "OpenCV loaded successfully");
+					mOpenCvCameraView.enableView();
+				}
+				break;
+				default: {
+					super.onManagerConnected(status);
+				}
+				break;
+			}
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+	}
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+
+	}
+
+	@Override
+	public Mat onCameraFrame(Mat inputFrame) {
+		return null;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+	}
+}
