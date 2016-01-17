54b4520dd4999cfd1a8dc668c6113bab559cce44
Mihail Kuznyetsov <ztaticvienn@gmail.com>
2016-01-17 10:13:53 +0200

    splash activity

--- openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/nnet/Documents/Projects/EyeAsisstant/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\ashhe\\AndroidProjects\\EyeAsisstant\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
